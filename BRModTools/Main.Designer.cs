a968809d0e3a02c539ca99bf32387ca6ea3ec9ed
Thomas Boby <th.boby@gmail.com>
2012-04-11 02:37:07 +0100

    Made Gabriel's Corrections,
    Add/remove material buttons.

---
+++ BRModTools/Main.Designer.cs
@@ -0,0 +1,227 @@
+﻿namespace BRModTools
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.selectMod = new System.Windows.Forms.Button();
+            this.remove = new System.Windows.Forms.Button();
+            this.create = new System.Windows.Forms.Button();
+            this.selectModBox = new System.Windows.Forms.ListBox();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.button3 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.button4 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.selectMod);
+            this.groupBox1.Controls.Add(this.remove);
+            this.groupBox1.Controls.Add(this.create);
+            this.groupBox1.Controls.Add(this.selectModBox);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(205, 449);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Mod Controls";
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(107, 315);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(91, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Save changes";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(125, 243);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Rename";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // selectMod
+            // 
+            this.selectMod.Location = new System.Drawing.Point(7, 243);
+            this.selectMod.Name = "selectMod";
+            this.selectMod.Size = new System.Drawing.Size(111, 23);
+            this.selectMod.TabIndex = 3;
+            this.selectMod.Text = "Select mod folder";
+            this.selectMod.UseVisualStyleBackColor = true;
+            this.selectMod.Click += new System.EventHandler(this.selectMod_Click);
+            // 
+            // remove
+            // 
+            this.remove.Enabled = false;
+            this.remove.Location = new System.Drawing.Point(107, 213);
+            this.remove.Name = "remove";
+            this.remove.Size = new System.Drawing.Size(92, 23);
+            this.remove.TabIndex = 2;
+            this.remove.Text = "Remove mod";
+            this.remove.UseVisualStyleBackColor = true;
+            this.remove.Click += new System.EventHandler(this.remove_Click);
+            // 
+            // create
+            // 
+            this.create.Enabled = false;
+            this.create.Location = new System.Drawing.Point(7, 213);
+            this.create.Name = "create";
+            this.create.Size = new System.Drawing.Size(94, 23);
+            this.create.TabIndex = 1;
+            this.create.Text = "Clone mod";
+            this.create.UseVisualStyleBackColor = true;
+            this.create.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // selectModBox
+            // 
+            this.selectModBox.FormattingEnabled = true;
+            this.selectModBox.Location = new System.Drawing.Point(7, 20);
+            this.selectModBox.Name = "selectModBox";
+            this.selectModBox.Size = new System.Drawing.Size(192, 186);
+            this.selectModBox.TabIndex = 0;
+            this.selectModBox.SelectedIndexChanged += new System.EventHandler(this.selectModBox_SelectedIndexChanged);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(224, 13);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(560, 448);
+            this.tabControl1.TabIndex = 1;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.button4);
+            this.tabPage1.Controls.Add(this.button3);
+            this.tabPage1.Controls.Add(this.dataGridView1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(552, 422);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Materials";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(7, 392);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 1;
+            this.button3.Text = "Add Material";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowDrop = true;
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToOrderColumns = true;
+            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(7, 7);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(539, 379);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(552, 422);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Textures";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(89, 392);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(96, 23);
+            this.button4.TabIndex = 2;
+            this.button4.Text = "Remove Material";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(796, 473);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Main";
+            this.Text = "BlockadeRunner Mod Tools";
+            this.groupBox1.ResumeLayout(false);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button selectMod;
+        private System.Windows.Forms.Button remove;
+        private System.Windows.Forms.Button create;
+        private System.Windows.Forms.ListBox selectModBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
---
+++ BRModTools/Main.cs
@@ -0,0 +1,384 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BRModTools
+{
+    public partial class Main : Form
+    {
+        string[] CLASS_TYPES = { "Helm", "Script", "Usable", "Standard", "Prefab", "PrefabFragment", "Power", "Propulsion", "Launcher", "Extra", "Paint", "Overlay", "Damage", "Void", "Null" };
+        string[] RENDER_TYPES = { "Standard", "Cylinder_Random", "CylinderYaw_Random", "CylinderYaw_Random2", "CylinderYaw_Random3", "Cylinder_Rarity", "Random", "Rarity", "Random3_Rarity", "Bulkhead", "Frame_Poxel", "Frame_Geometry", "Geometry", "Interior", "Plating", "None" };
+        string[] CATEGORY_TYPES = { "S_Bulkheads", "S_Plating", "S_Windows", "S_Transit", "S_Doors", "S_Natural", "H_Interiors", "H_Lights", "H_Terminals", "H_Furniture", "H_LifeSupport", "H_Hydroponics", "E_Maneuvering", "E_Mains", "E_Interstellar", "E_Docking", "D_Armor", "D_Shields", "D_Sensors", "D_Emitters", "D_Security", "W_Mounts", "W_Weapons", "W_Equipment", "W_Ammunition", "U_Power", "U_Cables", "U_Radiators", "U_Industrial", "None" };
+        ModList modList;
+        DataTable data;
+        Boolean first = true;
+        public Main()
+        {
+            InitializeComponent();
+        }
+        private void refreshSelect()
+        {
+            selectModBox.DataSource = modList.getModnames();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String selected = (String)selectModBox.SelectedValue;
+            modList.cloneMod(selected);
+            refreshSelect();
+        }
+
+        private void remove_Click(object sender, EventArgs e)
+        {
+            String selected = (String)selectModBox.SelectedValue;
+            modList.Remove(selected);
+            refreshSelect();
+        }
+
+        private void selectMod_Click(object sender, EventArgs e)
+        {
+            String exeLoc = Assembly.GetExecutingAssembly().Location;
+            FileInfo exe = new FileInfo(exeLoc);
+            DirectoryInfo dr = exe.Directory;
+            FileInfo brExe = new FileInfo(dr.FullName+@"\blockaderunner.exe");
+            FolderBrowserDialog folderBrowserDialog1 = new FolderBrowserDialog();
+            if (brExe.Exists) { folderBrowserDialog1.SelectedPath=dr.FullName+@"\mods"; }
+            folderBrowserDialog1.ShowDialog();
+            try
+            {
+                if (folderBrowserDialog1.SelectedPath != "")
+                {
+                    modFolderSelect(folderBrowserDialog1.SelectedPath);
+                }
+                button1.Enabled = true;
+                button2.Enabled = true;
+                create.Enabled = true;
+                remove.Enabled = true;
+                updateTable();
+             
+            }
+            catch (Exception)
+            {
+            }
+
+        }
+
+        private void modFolderSelect(String location)
+        {
+            modList = new ModList(location);
+            modList.init(); //init first so it can fail
+            selectModBox.DataSource = modList.getModnames();
+        }
+
+        private void selectModBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            updateTable();
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            string input = Microsoft.VisualBasic.Interaction.InputBox("Rename Mod", "Enter the new mod name");
+            if (!input.Equals(""))
+            {
+                String selected = (String)selectModBox.SelectedValue;
+                modList.rename(selected,input);
+            }
+            refreshSelect();
+        }
+
+        private void dataGridView1_CellValidating(object sender, DataGridViewCellValidatingEventArgs e)
+        {
+            /*if (e.ColumnIndex == myColumn.Index)
+            {
+                object eFormattedValue = e.FormattedValue;
+                if (!myColumn.Items.Contains(eFormattedValue))
+                {
+                    myColumn.Items.Add(eFormattedValue);
+                }
+            }*/
+        }
+        private void updateTable()
+        {
+            String selected = (String)selectModBox.SelectedValue;
+            data = modList.getTable(selected);
+            dataGridView1.DataSource = data;
+            if (first)
+            {
+                dataGridView1.Columns.Remove("Class");
+                dataGridView1.Columns.Remove("Category");
+                dataGridView1.Columns.Remove("Render");
+                DataGridViewComboBoxColumn comboboxColumn1;
+                DataGridViewComboBoxColumn comboboxColumn2;
+                DataGridViewComboBoxColumn comboboxColumn3;
+                comboboxColumn1 = CreateComboBoxColumn(1);
+                comboboxColumn2 = CreateComboBoxColumn(2);
+                comboboxColumn3 = CreateComboBoxColumn(3);
+                dataGridView1.Columns.Insert(1, comboboxColumn1);
+                dataGridView1.Columns.Insert(2, comboboxColumn2);
+                dataGridView1.Columns.Insert(8, comboboxColumn3);
+                dataGridView1.Columns[0].Frozen = true;
+                first = false;
+            }
+        }
+
+        private DataGridViewComboBoxColumn CreateComboBoxColumn(int columnNum) //1 = Cat, 2 Class, 3 Render
+        {
+            DataGridViewComboBoxColumn column =
+                new DataGridViewComboBoxColumn();
+            {
+                switch (columnNum)
+                {
+                    case 1:
+                        column.DataPropertyName = "Category";
+                        column.HeaderText = "Category";
+                        column.MaxDropDownItems = 3;
+                        column.Items.AddRange(CATEGORY_TYPES);
+                        column.ValueMember = "Category";
+                        column.DropDownWidth = DropDownWidth(CATEGORY_TYPES);
+
+                        break;
+                    case 2:
+                        column.DataPropertyName = "Class";
+                        column.HeaderText = "Class";
+                        column.MaxDropDownItems = 15;
+                        column.Items.AddRange(CLASS_TYPES);
+                        column.ValueMember = "Class";
+                        column.DropDownWidth = DropDownWidth(CLASS_TYPES);
+
+                        break;
+                    case 3:
+                        column.DataPropertyName = "Render";
+                        column.HeaderText = "Render";
+                        column.MaxDropDownItems = 29;
+                        column.Items.AddRange(RENDER_TYPES);
+                        column.ValueMember = "Render";
+                        column.DropDownWidth = DropDownWidth(RENDER_TYPES);
+
+                        break;
+                }
+            }
+            column.DisplayStyle = DataGridViewComboBoxDisplayStyle.Nothing;
+            return column;
+        }
+        //http://stackoverflow.com/questions/4842160/auto-width-of-comboboxs-content
+        int DropDownWidth(String[] items)
+        {
+            int maxWidth = 0;
+            int temp = 0;
+            Label label1 = new Label();
+
+            foreach (var obj in items)
+            {
+                label1.Text = obj.ToString();
+                temp = label1.PreferredWidth;
+                if (temp > maxWidth)
+                {
+                    maxWidth = temp;
+                }
+            }
+            label1.Dispose();
+            return maxWidth;   
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            String selected = (String)selectModBox.SelectedValue;
+            modList.Save(selected);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            DataRow row=data.Rows[0];
+            Object[] items = row.ItemArray;
+            items[0] = "CHANGEME";
+            data.Rows.Add(items);
+            dataGridView1.FirstDisplayedScrollingRowIndex = dataGridView1.Rows.Count - 1;
+            dataGridView1.Rows[(dataGridView1.Rows.Count - 1)].Selected = true;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Rows.Remove(dataGridView1.CurrentRow);
+        }
+    }
+
+
+    [Serializable()]
+    public class ModList
+    {
+        ModComparator myComparer = new ModComparator();
+        ArrayList mods = new ArrayList();
+        public ModList(String location)
+        {
+            DirectoryInfo directory = new DirectoryInfo(location);
+            DirectoryInfo[] modData = directory.GetDirectories();
+            foreach (DirectoryInfo mod in modData)
+            {
+                mods.Add(new Mod(mod.Name,mod.Parent.FullName));
+            }
+        }
+        public void init()
+        {
+            try
+            {
+                foreach (Mod mod in mods)
+                {
+                    mod.loadBlocks();
+                }
+            }
+            catch (FormatException)
+            {
+                throw new FormatException("ModList threw a format exception");
+            }
+        }
+
+        public void Save(String name)
+        {
+            Mod mod = getMod(name);
+            mod.Save();
+        }
+        public List<String> getModnames()
+        {
+            List<String> items = new List<String>();
+            foreach (Mod mod in mods)
+            {
+                items.Add(mod.Name);
+            }
+            return items;
+        }
+        public void cloneMod(String sourceString)
+        {
+            mods.Sort(myComparer);
+            Mod source = getMod(sourceString);
+            Mod clone = ObjectCopier.Clone<Mod>(source);
+            while(mods.BinarySearch(new Mod(sourceString,null),myComparer)>-1)
+            {
+                sourceString += "-c";
+            }
+            clone.Rename(sourceString);
+            mods.Add(clone);
+            clone.Save();
+        }
+        public void rename(String name, String newName)
+        {
+            Mod mod = getMod(name);
+            while (mods.BinarySearch(new Mod(newName, null), myComparer) > -1)
+            {
+                name += "-d";
+            }
+            mod.Name = newName;
+        }
+        public Mod getMod(String name)
+        {
+            Mod search = new Mod(name,null);
+            return (Mod)mods[mods.BinarySearch(search, myComparer)];
+        }
+        public void Remove(String name)
+        {
+            Mod mod = getMod(name);
+            mod.Delete();
+            mods.RemoveAt(mods.BinarySearch(mod,myComparer));
+        }
+        public DataTable getTable(String name)
+        {
+            Mod mod = getMod(name);
+            return mod.getTable();
+        }
+    }
+
+    public class ModComparator : IComparer
+    {
+
+        public int Compare(object x, object y)
+        {
+            Mod x2 = (Mod)x;
+            Mod y2 = (Mod)y;
+            String paramX = x2.Name;
+            String paramY = y2.Name;
+            return String.Compare(paramX, paramY);
+        }
+    }
+    [Serializable()]
+    public class Mod
+    {
+        String[] solidParams = { "Category", "Class", "DescLong", "DescShort", "Flags", "Geometry", "HP", "Mass", "Render", "Tags", "Texture" };
+
+        public String Name { get; set; }
+        private String location;
+        private DataTable Blocks;
+        public Mod(String name, String location)
+        {
+            this.Name=name;
+            this.location = location;
+        }
+        public void loadBlocks()
+        {
+            StreamReader stream = new StreamReader(location + @"\" + Name+@"\solids.xml");
+            try
+            {
+                Solids solids = ModParser.inputSolids(stream);
+                solids.TableCreater();
+                Blocks = solids.BlockTable;
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("The mod " + Name + " has an invalid .xml!");
+                throw new FormatException("Mod " + Name + " threw a formatexception");
+            }
+        }
+        public DataTable getTable()
+        {
+            return Blocks;
+        }
+        public void Rename(String name)
+        {
+            Name = name;
+
+            //TODO copy files
+        }
+        public Solids exportTable()
+        {
+            Solids output = new Solids();
+            foreach (DataRow row in Blocks.Rows)
+            {
+                if (!row[0].ToString().Equals(""))
+                {
+                    Block block = output.addBlock((String)row["Name"], solidParams);
+
+                    foreach (DataColumn column in Blocks.Columns)
+                    {
+                        block.setParam(column.Caption, (String)row[column.Caption]);
+                    }
+                }
+
+
+            }
+            return output;
+        }
+        public void Save()
+        {
+            Solids export = exportTable();
+            String xml = ModWriter.outputSolids(export);
+            System.IO.Directory.CreateDirectory(location + "\\" + Name);
+            FileStream fs = File.Create(location + @"\" + Name + @"\solids.xml");
+            StreamWriter file = new StreamWriter(fs);
+            //StreamWriter file = new StreamWriter(location + @"\" + Name + @"\solids.xml");
+            file.WriteLine(xml);
+            file.Close();
+         }
+        public void Delete()
+        {
+            File.Delete(location + @"\" + Name + @"\solids.xml");
+            Directory.Delete(location + "\\" + Name);
+        }
+    }
+}
